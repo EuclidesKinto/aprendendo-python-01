23fb6a9e2f77f4d7ff955d0d7a9d85c0805e718a
euclideskinto <euclideskinto@gmail.com>
2021-07-10 15:19:56 -0300

    Tipo string

---
+++ variaveis-e-tipo-de-dados/tipo_string.py
@@ -0,0 +1,16 @@
+"""
+
+Tipo string
+
+print(nome.split()) #Transforma em uma lista de string
+    - ['Python', 'Django']
+"""
+
+nome = "Python Django"
+
+# print(nome.split())
+# print(nome[0:6])
+# print(nome[0:9])
+# print(nome[::-1])
+# print(nome.replace('n', 'A'))
+
