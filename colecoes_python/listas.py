06c3455803320421155fdb302424940111a0fa6f
euclideskinto <euclideskinto@gmail.com>
2021-07-10 19:01:15 -0300

    Começando com listas

---
+++ colecoes_python/listas.py
@@ -0,0 +1,53 @@
+"""
+Listas: - funciona com vetores/matrizes/arrays como em outras liguagens, com a diferença de serem dinâmicos
+            e tbm de podermos colocar QUALQUER tipo de dado.
+
+        - DINÂMICO: Não possui tamanho fixo, ou seja, podemos criar a lista e simplesmente ir add elementos.
+        - Qualquer tipo de dado: Não possuem  tipo de dado fixo, ou seja, podemos add qualquer tipo de dado.
+        - As listas em python são representadas por colchetes: []
+
+"""
+lista1 = [1456, 2782, 2453, 456, 46, 75]
+
+lista2 = ['k', 'i', 'n']
+
+lista3 = []
+
+lista4 = list(range(11))
+
+lista5 = list('Brasil é o maior ganhador no mundo do futebol')
+
+# Podemos checar se um determinado valor está contido na lista
+# num = 18
+# if num in lista4:
+#     print(f'ok, o número {num} existe')
+# else:
+#     print(f'não tem o número {num}')
+
+#  ordenando lista comm sort()
+# lista1.sort()
+
+# reverter a ordem com reverse()
+# lista1.reverse()
+
+# Podemos facilmente contar o número de ocorrencia de um valor em uma lista
+# print(lista1.count(46))
+# print(lista5.count('r'))
+# Podemos adicionar elementos com o append e só pode add 1 elemento.
+# lista1.append('a')
+# print(lista1)
+# # podemos adicionar outra lista dentro de outra lista
+# lista1.append([23, 56, 45])
+# print(lista1)
+#
+# # extend() -> coloca o cada elemento individual e não aceita valor único
+# lista1.extend(['e', 'jo', 'ja'])
+# print(lista1)
+#
+# # podemos inserir um novo elemento informando a posição do indice
+# lista1.insert(1, 'Novo valor')
+# print(lista1)
+
+
+
+
