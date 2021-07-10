f69e3ad33e13bc9d0b596d746a5a3675d85303f8
euclideskinto <euclideskinto@gmail.com>
2021-07-10 15:30:42 -0300

    Escopo de variáveis

---
+++ variaveis-e-tipo-de-dados/escopo_de_variaveis.py
@@ -0,0 +1,17 @@
+"""
+
+Escopo de variáveis
+
+Dois casos de escopo:
+
+1 - Variaveis Globais:
+    - São reconhecidas TD programa
+
+2 - Locais:
+    - São reconhecida apenas no bloco onde foram declaradas, ou seja,
+        seu escopo está limitado ao bloco onde foi declarada
+
+Python é uma liguagem de tipagem dinâmica. Isso significa que ao
+declararmos uma variável, nós não colocamos o tipo de dado dela.
+Este tipo é inferido ao atribuirmos o valor a mesma.
+"""
