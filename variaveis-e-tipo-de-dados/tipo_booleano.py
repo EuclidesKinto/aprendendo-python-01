c0051d4df391996955aa4f428ed7c35e763da02e
euclideskinto <euclideskinto@gmail.com>
2021-07-10 14:54:04 -0300

    Tipo boolean

---
+++ variaveis-e-tipo-de-dados/tipo_booleano.py
@@ -0,0 +1,51 @@
+"""
+Tipo booleano
+
+True -> verdadeiro
+False -> Falso
+
+OBS: Sempre com a inicial maiúscula
+
+"""
+
+ativo = False
+print(ativo)
+
+
+"""
+Operações básicas
+NEGAÇÃO -> (not)
+
+Fazendo a negação, se o valor booleano for verdaeiro o resultado será falso e se for 
+falso o resultado será verdadeiro, ouseja, sempre o contrário do booleano
+
+"""
+print(not ativo)
+
+"""
+OU -> OR
+É uma operação binária, ou seja, depende de dois valores. Para ser 'TRUE', Um deles deve ser verdadeiro
+V or V -> V
+V or F -> V
+F or V -> V
+F or F -> F
+
+------------------------------------------------------------------------------------------------------
+
+E -> AND
+Também é uma operação binária. ou seja, depende de dois  valores. Para ser 'TRUE' os dois devem ser verdadeiros.
+V and V -> V
+V and F -> F
+F and V -> F
+F and F -> F
+
+
+"""
+
+
+
+
+
+
+
+
